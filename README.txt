7497a15070baa8a37db3b7b0084909cf032e8b84
Laird WT <laird.ts@gmail.com>
2023-04-02 15:22:28 +0100

    Added source files to repo

---
+++ README.txt
@@ -0,0 +1,6 @@
+2023 Laird Shingleton
+
+This code comes with no warrenty.
+
+Use: add any dcm files you wish to convert into the "input" folder. Then run the dicom_to_tga_converter.exe
+the new .exr files will be saved to the "output" folder
---
+++ src/main.rs
@@ -0,0 +1,84 @@
+use dicom_object::open_file;
+use dicom_pixeldata::PixelDecoder;
+use indicatif::{ProgressBar, ProgressStyle};
+use std::fs;
+
+fn main() {
+    // Get the current working directory
+    let cwd = std::env::current_dir().expect("Failed to get current directory");
+
+    // Define the input and output folder paths
+    let input_folder = cwd.join("input");
+    let output_folder = cwd.join("output");
+
+    // Create the output folder if it doesn't exist
+    if !output_folder.exists() {
+        fs::create_dir(&output_folder).expect("Failed to create output folder");
+    }
+
+    // Get the list of DICOM files in the input folder
+    let dcm_files = fs::read_dir(&input_folder)
+        .expect("Failed to read input folder")
+        .filter_map(|entry| {
+            if let Ok(entry) = entry {
+                if entry.file_type().unwrap().is_file()
+                    && entry.path().extension().map_or(false, |ext| ext == "dcm")
+                {
+                    Some(entry.path())
+                } else {
+                    None
+                }
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<_>>();
+
+    // Initialize the progress bar
+    let progress_bar = ProgressBar::new(dcm_files.len() as u64);
+    progress_bar.set_style(
+        ProgressStyle::default_bar()
+            .template("{percent}% [{bar:40.cyan/blue}] {pos}/{len} {msg}").expect("Progress bar template not found")
+            .progress_chars("#>-"),
+    );
+
+    // Process each DICOM file
+    for file_path in dcm_files {
+        let file_name = file_path
+            .file_stem()
+            .expect("Failed to get file stem")
+            .to_string_lossy()
+            .to_string();
+
+        let output_path = output_folder.join(file_name + ".exr");
+
+        // Decode the pixel data and save the output image
+        match open_file(&file_path) {
+            Ok(obj) => {
+                match obj.decode_pixel_data() {
+                    Ok(pixel_data) => {
+                        let dynamic_image =
+                            pixel_data.to_dynamic_image(0).expect("Failed to convert to dynamic image");
+                        let rgba_image = dynamic_image.to_rgba32f();
+                        rgba_image.save_with_format(&output_path, image::ImageFormat::OpenExr)
+                            .expect("Failed to save image");
+                        progress_bar.inc(1);
+                    }
+                    Err(err) => {
+                        println!("Failed to decode pixel data from file {}: {}", file_path.display(), err);
+                        progress_bar.abandon_with_message("Aborted due to error");
+                        return;
+                    }
+                }
+            }
+            Err(err) => {
+                println!("Failed to open file {}: {}", file_path.display(), err);
+                progress_bar.abandon_with_message("Aborted due to error");
+                return;
+            }
+        }
+    }
+
+    // Finish the progress bar
+    progress_bar.finish_with_message("Done");
+}
